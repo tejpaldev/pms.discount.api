f3b6f2a1b086ff75b1a55107309623c9477e2d02
Tejpal Singh <tejpal.dev@outlook.com>
2025-05-16 11:39:03 +0530

    okn

--- Infrastructure/EventBus.Messages/Common/EventBusConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace EventBus.Messages.Common;
-
-public class EventBusConstants
-{
-    //This queue name will come in rabbit mq management portal
-    public const string BasketCheckoutQueue = "basketcheckout-queue";
-    public const string BasketCheckoutQueueV2 = "basketcheckout-queue-v2";
-}
